269cce29203a0112b7e534a5a359526b5d5bce7c
Melih karakuş <melih.karakus1818@gmail.com>
2024-04-01 18:25:57 +0300

    MenuController oluşturuldu. Menu ındex ayarlandı. Ajax kullanımı yapıldı.

---
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ProductDto;
+
+namespace SignalRWebUI.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7123/api/Product");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ SignalRWebUI/ViewComponents/MenuComponents/_MenuFooterComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ContactDto;
+
+namespace SignalRWebUI.ViewComponents.MenuComponents
+{
+    public class _MenuFooterComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _MenuFooterComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7123/api/Contact");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
